a3a92dd0a6865350edefe4f6e923fc7a34a42cb0
Alexander Ukhin <55115895+DWN013@users.noreply.github.com>
2023-06-17 00:29:50 -0400

    Global number of threads check

---
+++ Multithreading practice/global-num-threads.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <omp.h>
+int main() {
+    omp_set_num_threads(8);
+    #pragma omp parallel
+     {
+        int thread_id = omp_get_thread_num();
+        int n_threads = omp_get_num_threads();
+        if (thread_id == 0) printf("There are %d threads\n", n_threads);
+     }
+return 0;
+}
